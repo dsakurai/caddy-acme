d1fbca8e3ed07d23ccc6d6d2f0a6332cb4d48d2b
Daisuke Sakurai <d.sakurai0@gmail.com>
2025-05-05 14:47:18 +0900

    Fix the health check.

--- up.sh
+++ up.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
 podman-compose --podman-run-args=--replace up -d caddy-ca
-sleep 3 # Wait for the caddy-ca to start and serve the root certificate.
+
+echo "Waiting for the root certificate to be generated..."
+
+while [ ! -f ./caddy_data/caddy/pki/authorities/local/root.crt ]
+do 
+	sleep 1 # Wait for the caddy-ca to start and serve the root certificate.
+done
+
+echo "The root certificate is generated."
+
 podman-compose --podman-run-args=--replace up -d caddy-leaf
+
